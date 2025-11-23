2d894c33762f44369b74c20a90e819caf83484a8
Jeremy Komarov <jeremy@boostapp.com>
2025-11-23 21:03:27 +0200

    OM-5-JK: fixed controller anotaionts

--- src/main/java/com/firstProject/controller/CustomerOrderController.java
+++ src/main/java/com/firstProject/controller/CustomerOrderController.java
@@ -3,10 +3,9 @@ package com.firstProject.controller;
 import com.firstProject.model.CustomerOrder;
 import com.firstProject.service.CustomerOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/order")
 public class CustomerOrderController {
 
